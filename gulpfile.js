c700ef10692a7030607c5431593a6758c66d1678
TONI CAKTAS <TONICAKTAS@YAHOO.COM>
2016-08-31 17:34:13 +0200

    gulpfilen1

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,54 @@ var watch = require('gulp-watch');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var browserSync = require('browser-sync');
+var  maps = require('gulp-sourcemaps'); // map orientering för leta upp cod i dev-tool
+var del = require('del');             // radera filer
+var rename = require('gulp-rename');   // för att behålla snygga versionen av concat när man gör uglify
+
 
 gulp.task('sass', function() {
 
   return gulp.src('app/sass/style.scss')
-
+    .pipe(maps.init())
     .pipe(sass())
+    .pipe(maps.write('./'))  // skriver maps filen i folder den beffiner sig i
     .pipe(gulp.dest('public/css'))
     .pipe(browserSync.reload({stream:true}))
 
 });
 
-gulp.task('uglify', function() {
-
-	return gulp.src('app/js/*.js')
-		.pipe(concat('all.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('public/scripts'))
-
-
+gulp.task("concatScripts", function() { //lägger ihop alla scripts till en concat.js
+    return gulp.src([
+        'app/js/*.js'
+        ])
+    .pipe(maps.init())
+    .pipe(concat('concat.js'))  // snygag versionen av js filer
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('public/scripts'));
 });
+
+gulp.task("uglify", ["concatScripts"], function() { //tar bort whitespace,preformance related(not necisary for development)
+  return gulp.src("public/scripts.concat.js")
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))       // uglify versionen
+    .pipe(gulp.dest('public/scripts'));
+});
+
+
+
+
+gulp.task("production", ['uglify', 'sass'],function(){ //lägger ihop fler tasks
+  return gulp.src(['public/scripts/all.min.js','public/index.html','public/css/style.css'],{base:'./'})  // compila ihop hella appen i en färdig mapp 'dist'
+                    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean',function(){ // delita all filer som var compilade by gulp
+  del(['dist','public/css/style.css*','public/scripts/all.*.js*']);
+})
+
+
+
+
 
 gulp.task('watch', function() {
 
---
+++ public/scripts/concat.js
@@ -0,0 +1,9 @@
+var msg = "hejsan";
+
+console.log(msg);
+
+var name = "AJK15G";
+
+console.log(name);
+
+//# sourceMappingURL=concat.js.map
