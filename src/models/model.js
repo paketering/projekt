dbb0358b69ebdcffd4a611e97e5efab5a751fcfe
Tomas Johansson <tomtomjohansson@yahoo.se>
2016-08-31 11:52:47 +0200

    Added model, database-connection and sample-objects in seed-file

---
+++ src/models/model.js
@@ -0,0 +1,14 @@
+'use strict';
+const mongoose = require('mongoose');
+
+const subjectSchema = new mongoose.Schema({
+   name: {type: String, unique: true},
+   prio: {type:Number,'default':1},
+   status: {type:Number,'default':1},
+   links: [String],
+   notes: String
+});
+
+const subjectModel = mongoose.model('Subject', subjectSchema);
+
+module.exports = subjectModel;
---
+++ src/seed.js
@@ -0,0 +1,39 @@
+let Subject = require('./models/model.js');
+
+var react = new Subject();
+react.name = 'ReactJS';
+react.prio = 1;
+react.status = 1;
+react.links = ["https://facebook.github.io/react-native/docs/tutorial.html","http://stackoverflow.com/questions/tagged/reactjs"];
+react.notes = "Franzen portland authentic, semiotics thundercats gochujang tofu. Try-hard actually fingerstache narwhal, helvetica cred readymade ennui freegan VHS 90's yuccie schlitz small batch. Locavore ae.";
+
+var grunt = new Subject();
+grunt.name = 'GruntJS';
+grunt.prio = 3;
+grunt.status = 2;
+grunt.links = ["http://gruntjs.com/"];
+grunt.notes = "Franzen portland authentic, semiotics thundercats gochujang tofu. Try-hard actually fingervore ae.";
+
+var jquery = new Subject();
+jquery.name = 'jQuery';
+jquery.prio = 2;
+jquery.status = 3;
+jquery.links = ["http://jquery.com/","https://code.jquery.com/"];
+jquery.notes = " Distillery cornhole plaid slow-carb whatever, actually umami taxidermy locavore. Mixtape ramps pabst post-ironic. Readymade keytar selvage lo-fi, organic photo booth distillery fanny pack thundercats banh dollar toast.";
+
+var mongo = new Subject();
+mongo.name = 'MongoDB';
+mongo.prio = 2;
+mongo.status = 1;
+mongo.links = ["http://mongoosejs.com/docs/subdocs.html"];
+mongo.notes = "Franzen portland authentic, semiotics thundercats gochujang tofu. Try-hard actually fingervore ae. sadfkjsldk s ldkfjsldkf.";
+
+let subjects = [react,grunt,jquery,mongo];
+subjects.forEach((subject,index)=>{
+   User.find({name:subject.name},(err, subjects)=>{
+      if(!err && !subjects.length){
+         Subject.create(subject);
+         console.log(subject.name);
+      }
+   });
+});
