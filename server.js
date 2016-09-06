2b5c4bfcd523c40320749446da7e5e6d8136c817
Tomas Johansson <tomtomjohansson@yahoo.se>
2016-09-06 14:01:33 +0200

    Added express server

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+var express = require('express');
+var app = express();
+var path = require('path');
+var parser = require('body-parser');
+var PORT = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var api = require('./src/api');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(parser.json());
+app.use('/api', api);
+
+app.get('/*', (req, res,next)=>{
+   res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(PORT,(err)=>{
+   if(err){
+      console.log('Failed to connect');
+   }
+   else{
+      console.log('Connected to server '+PORT);
+   }
+});
