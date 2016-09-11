5140be5d5ffc0f4d29a14a694e06d0231ecf38a8
saman movaghar <saman_mov@yahoo.com>
2016-09-11 23:21:06 +0200

    fixed index

--- app/app.js
+++ app/app.js
@@ -4,3 +4,27 @@ const angular =require('angular');
 var MainController = require('./js/MainController.js')
 var service = require('./js/service.js')
 var app = angular.module('app', [MainController.name, service.name]);
+
+var routerApp = angular.module('routerApp', ['ui.router']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/index');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('index', {
+            url: '/index',
+            templateUrl: 'public/index.html'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+        	url: '/update',
+        	 templateUrl: 'public/update.html',
+        	 controller: ''
+            // we'll get to this in a bit       
+        });
+        
+});
